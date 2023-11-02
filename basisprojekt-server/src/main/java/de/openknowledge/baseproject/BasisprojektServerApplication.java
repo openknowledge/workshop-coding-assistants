fa9a48b4a3fb3f8e2ba5d4025be9f2e20751d1bc
Arne Limburg <arne.limburg@openknowledge.de>
2023-11-02 17:47:41 +0100

    Initial setup

---
+++ basisprojekt-server/src/main/java/de/openknowledge/baseproject/BasisprojektServerApplication.java
@@ -0,0 +1,13 @@
+package de.openknowledge.baseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasisprojektServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasisprojektServerApplication.class, args);
+	}
+
+}
---
+++ basisprojekt-server/src/test/java/de/openknowledge/baseproject/BasisprojektServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.openknowledge.baseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasisprojektServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
