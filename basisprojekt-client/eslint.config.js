7bb3855e050a636be64ba02eb024c0af01ce2e8a
Sören Dierkes <soeren.dierkes@openknowledge.de>
2025-11-17 07:32:40 +0000

    Add prettier and linting documentation

--- basisprojekt-client/eslint.config.js
+++ basisprojekt-client/eslint.config.js
@@ -1,12 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+    'dist',
+    'node_modules',
+    'build',
+    'coverage',
+    '*.config.js',
+    '*.config.ts',
+    'public',
+  ]),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -20,4 +28,4 @@ export default defineConfig([
       globals: globals.browser,
     },
   },
-])
+]);
